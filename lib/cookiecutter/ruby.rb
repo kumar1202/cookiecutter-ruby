3b456557a32d4da405729654d7c406f4f043bd18
Kumar Abhijeet <kumarabhijeet1202@gmail.com>
2020-11-02 16:16:52 +0530

    Bootstrap gem

---
+++ lib/cookiecutter/ruby.rb
@@ -0,0 +1,8 @@
+require "cookiecutter/ruby/version"
+
+module Cookiecutter
+  module Ruby
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/cookiecutter/ruby_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Cookiecutter::Ruby do
+  it "has a version number" do
+    expect(Cookiecutter::Ruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
